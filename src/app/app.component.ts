da0ac6062549dd4d4783281250cd2c70002a095f
Pratik Kumar <pratik007kumar@gmail.com>
2017-05-17 15:33:08 +0530

    master details example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,17 +27,53 @@ const CONTACTS: Contact[] = [
   			<h1>{{title}}</h1>
   			<div class="col-lg-6">
   			<ul class="contact-list">
-			<li *ngFor="let contact of contacts">
+			<li *ngFor="let contact of contacts"
+	        [class.selected]="contact === selectedContact"
+	        (click)="onSelect(contact)"
+			>
 				{{contact.name}}
 			</li>
   			</ul>
   			</div>
+
+  			    <div class="col-lg-6" *ngIf="selectedContact">
+			      <div class="well">
+			      <div class="row">
+
+			      <h2>{{selectedContact.name}} details!</h2>
+			      <div class="col-lg-6">
+	  				<div class="form-group">
+	  				<label class="label label-default" >Name:</label>
+	  				<br>
+	  				<br>
+	  				<input [(ngModel)]="selectedContact.name" placeholder="name" class="form-control">
+	  				</div>
+
+	  				<div class="form-group">
+
+	  				<label class="label label-default">Mobile:</label>
+	  				<br>
+	  				<br>
+	  				<input [(ngModel)]="selectedContact.mobile" class="form-control" placeholder="name">
+	  				</div>
+
+	  				<div class="form-group ">
+	  				<label class="label label-default">Email:</label>
+	  				<br>
+	  				<br>
+	  				<input type="email" [(ngModel)]="selectedContact.email" class="form-control" placeholder="name">
+	  				</div>
+	  				</div>
+	  				</div>
+
+			    </div>
   			</div>
   	`,
  styles:[`
  	.contact-list{ list-style:none}
  	.contact-list li{ border:#ccc 1px solid; padding:5px; background:#eee; margin:5px;}
- 	.contact-list li:hover{ border:#f00 1px solid; padding:5px; margin:5px; color:#fff; background:#999;}
+ 	.contact-list li:hover{ border:#f00 1px solid; padding:5px; margin:5px;background:#999;}
+ 	.selected{ border:#f00 1px solid !important; padding:5px !important; margin:5px !important;background:#999 !important;}
  `] 	
 })
 
@@ -45,4 +81,9 @@ export class AppComponent  {
 	title = 'Angular Test contact';
 
 	contacts= CONTACTS;
+	selectedContact: Contact;
+
+	onSelect(contact: Contact): void {
+	  this.selectedContact = contact;
+	}
 }
