7e85f4872a1205bf27cc70f834d61b3a605389d9
Pratik Kumar <pratik007kumar@gmail.com>
2017-05-17 14:37:23 +0530

    the hero deditor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,47 @@
 import { Component } from '@angular/core';
 
+export class Contact {
+  id: number;
+  name: string;
+  email: string;
+  mobile: string;
+  
+}
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<div class="container">
+  			<h1>{{title}}</h1>
+	  			<div class="row">
+	  				<div class="col-lg-6">
+	  				<div class="form-group">
+	  				<label class="label label-default" >Name:</label>
+	  				<input [(ngModel)]="contact.name" placeholder="name" class="form-control">
+	  				</div>
+
+	  				<div class="form-group">
+	  				<label class="label">Mobile:</label>
+	  				<input [(ngModel)]="contact.mobile" class="form-control" placeholder="name">
+	  				</div>
+
+	  				<div class="form-group">
+	  				<label class="label">Email:</label>
+	  				<input type="email" [(ngModel)]="contact.email" class="form-control" placeholder="name">
+	  				</div>
+
+ 
+
+	  				</div>
+	  			</div>
+  			</div>
+  	`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Angular Test contact';
+
+
+contact: Contact ={
+			id:1,name:'pratik',mobile:"9708213024",email:"pratik007kumar@gmail.com"
+			};
+	 }
